5c58e727503c936e3a11052171ddae07f49bd146
lucas.g <lucas.garin73@gmail.com>
2017-11-20 17:12:06 +0100

    fiw file

--- src/htm/HTM.java
+++ src/htm/HTM.java
@@ -4,18 +4,31 @@
  * and open the template in the editor.
  */
 package htm;
+
+
+import graph.graphstream.GraphStreamBuilder;
+import graph.graphstream.MyGraphStreamEdge;
+import graph.graphstream.MyGraphStreamNode;
+import org.graphstream.graph.EdgeFactory;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.NodeFactory;
+import org.graphstream.graph.implementations.AbstractGraph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
- *
  * @author farmetta
  */
 public class HTM {
 
-    
+
     /** TODO
      *  Terminer la construction du réseau
      *  Dessiner suivant le type (carré et rond)
@@ -23,27 +36,38 @@ public class HTM {
      *  Préparer prototypes fonctions
      *  Connecter un graphe
      */
-    
-    
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
 
-        String fileName = "files.txt";
+        String fileName = "valueIn.txt";
+        List<List<Boolean>> valuesIn = new ArrayList<>();
 
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-
-            stream.forEach(System.out::println);
+            Object[] v = stream.toArray();
+            String tmpBool;
+            List<Boolean> listBool;
+            for (int i = 0; i < v.length; i++) {
+                listBool = new ArrayList<>();
+                tmpBool = (String) v[i];
+                char[] charArray = tmpBool.toCharArray();
+                for (int j = 0; j < charArray.length; j++) {
+                    listBool.add(charArray[j] == '1');
+                }
+                valuesIn.add(listBool);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-/*
-        Graph graph = new SingleGraph("graph"); // création du graphe
+        Graph graph; // création du graphe
+        graph = new SingleGraph("graph");
         graph.setNodeFactory(new NodeFactory<MyGraphStreamNode>() {
 			public MyGraphStreamNode newInstance(String id, Graph graph) {
 				return new MyGraphStreamNode((AbstractGraph) graph, id); // les noeuds seront de type MyGraphStreamNode
@@ -61,7 +85,7 @@ public class HTM {
 		});
         
         GraphStreamBuilder gb = new GraphStreamBuilder(graph);
-        MyNetwork mn = new MyNetwork(gb, gb);
+        MyNetwork mn = new MyNetwork(valuesIn, gb, gb);
         
         mn.buildNetwork(16, 5); // un réseau de démonstration avec 16 entrées et 5 colonnes
         graph.display(false);
@@ -70,8 +94,7 @@ public class HTM {
         new Thread(mn).start(); // exécution d'un processus d'apprentissage, à définir, pour mn
         
         
-        */
-        
+
     }
-    
+
 }
