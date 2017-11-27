35af6913256f9cd0e17962cb7794abf7ceea28b5
lucas.g <lucas.garin73@gmail.com>
2017-11-27 14:16:20 +0100

    fix conflict

--- src/htm/MyNetwork.java
+++ src/htm/MyNetwork.java
@@ -67,7 +67,7 @@ public class MyNetwork implements Runnable {
             
             if (!n.getNode().isConnectedTo(c.getNode())) {
                 EdgeInterface e = eb.getNewEdge(n.getNode(), c.getNode());
-                MySynapse s = new MySynapse(e);
+                MySynapse s = new MySynapse(e, n);
                 c.setSynapse(s);
                 e.setAbstractNetworkEdge(s);
                 
